08f497a03456cb094bb7089cbf0838261d5fe2d2
Igor Sverchkov <knjaz1989@gmail.com>
2023-10-02 15:30:31 +0300

    h2 t2 finished

--- event-object/keysolo/task.js
+++ event-object/keysolo/task.js
@@ -25,6 +25,17 @@ class Game {
       При неправильном вводе символа - this.fail();
       DOM-элемент текущего символа находится в свойстве this.currentSymbol.
      */
+    document.addEventListener(
+      "keydown", 
+      (event) => {
+        let currentSymbol = this.currentSymbol;
+        if (currentSymbol.textContent === event.key.toLowerCase()) {
+          this.success();
+        } else {
+          this.fail();
+        }
+      }
+    );
   }
 
   success() {
