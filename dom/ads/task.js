ea3b6e8f88a924ac2a4d6076101417e0a129672f
Igor Sverchkov <knjaz1989@gmail.com>
2023-10-15 21:44:48 +0300

    h3 t2 modified

--- dom/ads/task.js
+++ dom/ads/task.js
@@ -25,24 +25,31 @@
 
 // Task 2
 
+timer = (childsArray) => {
+  const firstElenent = childsArray.shift();
+  const secondElenent = childsArray[0];
+  const color = secondElenent.getAttribute("data-color");
+  const dataSpeed = secondElenent.getAttribute("data-speed");
+  setTimeout(
+    () => {
+      firstElenent.classList.remove("rotator__case_active");
+      secondElenent.style.color = color;
+      secondElenent.classList.add("rotator__case_active");          
+      childsArray.push(firstElenent);
+      timer(childsArray);
+    }, 
+    dataSpeed
+    )
+}
+
+
 startRotator = (className) => {
   const rotator = document.querySelectorAll(className);
   rotator.forEach(element => 
     {
       const childs = element.children;
       let childsArray = Array.from(childs);
-      setInterval(
-        () => {
-          const firstElenent = childsArray.shift();
-          const secondElenent = childsArray[0];
-          const color = secondElenent.getAttribute("data-color");
-          firstElenent.classList.remove("rotator__case_active");
-          secondElenent.style.color = color;
-          secondElenent.classList.add("rotator__case_active");          
-          childsArray.push(firstElenent);
-        },
-        1000
-      )
+      timer(childsArray);
     }  
   )
 };
